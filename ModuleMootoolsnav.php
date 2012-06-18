79948bc19f74ebc895a08f28048c089de18307c8
Jan Reuteler <jan.reuteler@iserv.ch>
2012-06-18 15:18:45 +0200

    2.0

--- ModuleMootoolsnav.php
+++ ModuleMootoolsnav.php
@@ -1,13 +1,15 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
 
 /**
- * TYPOlight webCMS
- * Copyright (C) 2005 Leo Feyer
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,68 +18,18 @@
  * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
- * Software Foundation website at http://www.gnu.org/licenses/.
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  Andreas Schempp 2008
+ * @copyright  Andreas Schempp 2008-2012
  * @author     Andreas Schempp <andreas@schempp.ch>
- * @license    LGPL
+ * @author     Jan Reuteler <jan.reuteler@iserv.ch> 
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
 
-class ModuleMootoolsnav extends Module
+class ModuleMootoolsnav extends ModuleNavigation
 {
 
-	/**
-	 * Template
-	 * @var string
-	 */
-	protected $strTemplate = 'mod_mootoolsnav';
-	
-	protected $navigationTpl = 'nav_moomenu';
 
-
-	/**
-	 * Do not display the module if there are no menu items
-	 * @return string
-	 */
-	public function generate()
-	{
-		if (TL_MODE == 'BE')
-		{
-			$objTemplate = new BackendTemplate('be_wildcard');
-			$objTemplate->wildcard = '### MOOTOOLS NAVIGATION MENU ###';
-
-			return $objTemplate->parse();
-		}
-
-		$strBuffer = parent::generate();
-		return strlen($this->Template->items) ? $strBuffer : '';
-	}
-
-
-	/**
-	 * Generate module
-	 */
-	protected function compile()
-	{
-		global $objPage;
-		
-		$GLOBALS['TL_JAVASCRIPT']['moomenu'] = 'system/modules/mootoolsnav/html/moomenu.js';
-
-		$trail = $objPage->trail;
-		$level = ($this->levelOffset > 0) ? $this->levelOffset : 0;
-
-		// Overwrite with custom reference page
-		if ($this->defineRoot && $this->rootPage > 0)
-		{
-			$trail = array($this->rootPage);
-			$level = 0;
-		}
-
-		$this->Template->skipId = 'skipNavigation_' . $this->id;
-		$this->Template->request = ampersand($this->Environment->request, ENCODE_AMPERSANDS);
-		$this->Template->skipNavigation = specialchars($GLOBALS['TL_LANG']['MSC']['skipNavigation']);
-		$this->Template->items = $this->renderNavigation($trail[$level]);
-	}
 }
