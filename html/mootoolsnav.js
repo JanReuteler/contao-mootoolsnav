7705f095883d152f81d8f17dcc4cf229368feed9
Jan Reuteler <jan.reuteler@terminal42.ch>
2013-05-03 18:10:37 +0200

    wip

--- html/mootoolsnav.js
+++ html/mootoolsnav.js
@@ -13,9 +13,9 @@ var MootoolsNav = new Class({
 	initialize: function(container, options)
 	{
 		this.setOptions(options);
-		
+
 		this.container = document.getElement(container);
-		
+
 		if (this.options.event == 'click')
 		{
 			this.eventElements = this.container.getElements(('.level_' + this.options.level + ' > li.submenu > a, .level_' + this.options.level + ' > li.submenu > span'));
@@ -30,40 +30,52 @@ var MootoolsNav = new Class({
 				'mouseleave': this.close
 			});
 			this.submenus = this.eventElements.getChildren('ul');
+			console.log(this.eventsElements);
 		}
-		
-		this.submenus.each(function (submenu) 
+
+		this.submenus.each(function (submenu)
 		{
-			submenu.set('slide', { mode: 'vertical', duration: 500, resetHeight:true, link:'cancel'  }).slide('hide');
+			//alert(options.direction);
+			if(options.direction == 'bottom') {
+				submenu.set('slide', { mode: 'vertical', duration: 500, resetHeight:true, link:'cancel'  }).slide('hide');
+
+			} else if(options.direction == 'right') {
+				submenu.set('slide', { mode: 'horizontal', duration: 500, resetHeight:true, link:'cancel'  }).slide('hide');
+
+			}
+			//submenu.set('slide', { mode: 'horizontal', duration: 500, resetHeight:true, link:'cancel'  }).slide('hide');
+			//submenu.slide('hide');
+
 		}.bind(this));
+
 	},
-	
+
 	toggle: function(event)
 	{
 /*
 		var el = event.target,
-			
-			
+
+
 		if (!submenu)
 			return;
-		
+
 */
-		
-		
-		
+
+
+
 		return false;
 	},
-	
+
 	open: function(event)
 	{
 		var submenu = this.getSubmenu(event.target);
-		
+
 		if (submenu)
 		{
 			if (!this.options.keepOpen)
 			{
 				var current = this.submenus.indexOf(submenu);
-				
+
 				for( var i=0; i<this.submenus.length; i++ )
 				{
 					if (i != current)
@@ -73,31 +85,30 @@ var MootoolsNav = new Class({
 					}
 				}
 			}
-			
+
 			console.log('open');
 			submenu.slide('in');
 		}
-		
+
 		return false;
 	},
-	
+
 	close: function(event)
 	{
 		var submenu = this.getSubmenu(event.target);
-		
+
 		if (submenu)
 		{
 			submenu.slide('out');
 			return false;
 		}
 	},
-	
+
 	getSubmenu: function(el)
 	{
 		return el.get('tag') == 'li' ? el.getElement('ul') : el.getNext('div > ul');
 	}
 });
- 
- 
- 
- 
+
+
+
